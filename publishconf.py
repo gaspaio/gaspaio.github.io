8e2bcca4eea8b238277484aad04c6552863acb61
Gaspaio <ggaspaio@gmail.com>
2015-10-25 15:47:31 +0100

    disqus + GA

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "gaspaio-github"
+GOOGLE_ANALYTICS = "UA-69279501-1"
